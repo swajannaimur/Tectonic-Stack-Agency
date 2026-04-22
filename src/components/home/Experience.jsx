b46733558b181fdc3fb54cb23a730dcc885f083a
swajannaimur@gmail.com <swajannaimur@gmail.com>
2026-04-22 22:58:16 +0600

    Added first files

---
+++ src/components/home/Experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <div>
+            <h2>he</h2>
+        </div>
+    );
+};
+
+export default Experience;
---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Experience from "./Experience";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Hero Section</h1>
+      <Experience />
+    </div>
+  );
+};
+
+export default Hero;
